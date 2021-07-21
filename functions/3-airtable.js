b778b1e5442d5bac5b27cbb6b6d10064b3dd496b
Aljosa <aljosaa.njezic@gmail.com>
2021-07-21 15:05:32 +0200

    add new

--- functions/3-airtable.js
+++ functions/3-airtable.js
@@ -1,32 +1,31 @@
 require('dotenv').config()
-
-const Airtable = require('airtable-node')
+const Airtable = require('airtable-node');
  
 const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
-  .base('appo69CddA9EGoOUl')
+  .base('appyRPUiWdEB5KJ7v')
   .table('products')
 
 
 
-
-exports.handler = async (event , context,cb) => {
- try {
-  const {records} = await airtable.list()
+exports.handler = async(event,context,cb) => {
+try {
+ const {records} = await airtable.list()
  const products = records.map((product) => {
   const {id} = product;
   const {name,image,price} = product.fields
   const url = image[0].url
   return {id,name,url,price}
  })
- return {
-   statusCode: 200,
+  return {
+ statusCode: 200,
   body: JSON.stringify(products),
  }
- } catch (error) {
-   return {
-   statusCode: 500,
+} catch (error) {
+  return {
+ statusCode: 500,
   body: 'Server Error',
  }
- }
+}
  
+
 }
--- functions/3-product.js
+++ functions/3-product.js
@@ -1,38 +1,35 @@
 require('dotenv').config()
-
-const Airtable = require('airtable-node')
+const Airtable = require('airtable-node');
  
 const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
-  .base('appo69CddA9EGoOUl')
+  .base('appyRPUiWdEB5KJ7v')
   .table('products')
 
-
-
-exports.handler = async (event , context,cb) => {
- const {id} = event.queryStringParameters
- if(id) {
-try {
- const product = await airtable.retrieve(id)
- if(product.error){
-   return {
-  statusCode: 404,
-  body: `No product with id: ${id}`,
- }
- }
- return {
-  statusCode: 200,
-  body: JSON.stringify(product),
- }
-} catch (error) {
- return {
-  statusCode: 500,
-  body: `Server Error`,
- }
+exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters
+  if (id) {
+    try {
+      const product = await airtable.retrieve(id)
+      if (product.error) {
+        return {
+          statusCode: 404,
+          body: `No product with id: ${id}`,
+        }
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: `Server Error`,
+      }
+    }
+  }
+  return {
+    statusCode: 400,
+    body: 'Please provide product id',
+  }
 }
- 
- }
- return {
-  statusCode: 400,
-  body: 'please provide product id',
- }
-}
+
